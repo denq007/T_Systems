635052c904551dc4228bd326a26b81b2468100a1
DenisMurashko <dennn87@mail.ru>
2021-05-29 19:00:19 +0300

    add queue and change server

---
+++ src/main/java/com/t_systems/ecare/eCare/services/MessageSenderImpl.java
@@ -0,0 +1,40 @@
+package com.t_systems.ecare.eCare.services;
+
+import org.apache.log4j.Logger;
+
+import javax.annotation.Resource;
+import javax.jms.*;
+
+public class MessageSenderImpl implements MessageSender{
+
+    private static final long TIME_TO_LIVE_MILLISEC = 30_000; //messages older than that value won't be delivered to destination
+
+    @Resource(lookup = "java:/eCare/MyConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Resource(lookup = "java:/eCare/MyQueue")
+    private Destination destination;
+
+    /**
+     * Send message to JMS queue
+     *
+     * @param json message content in json formatted String
+     */
+    @Override
+    public void sendMessage(String json) {
+        try (
+                QueueConnection connection = (QueueConnection) connectionFactory.createConnection();
+                QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+                MessageProducer producer = session.createProducer(destination)
+        ) {
+
+            TextMessage message = session.createTextMessage(json);
+            producer.setTimeToLive(TIME_TO_LIVE_MILLISEC);
+            producer.send(message);
+            Logger.getLogger(MessageSenderImpl.class).info("Send jms:" + json);
+
+        } catch (JMSException ex) {
+            Logger.getLogger(MessageSenderImpl.class).error(ex.getMessage(), ex);
+        }
+    }
+}
